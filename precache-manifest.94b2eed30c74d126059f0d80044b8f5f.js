8369ba87498d867b533a5c51a218261f3062685a
i_am_yevhen <yevheniiperekopskyi@gmail.com>
2020-06-16 11:53:50 +0300

    Updates

---
+++ precache-manifest.94b2eed30c74d126059f0d80044b8f5f.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c10370d6a2db655085750f8a8825d486",
+    "url": "/Dota2-API/index.html"
+  },
+  {
+    "revision": "d1d1e49540916fd05ba1",
+    "url": "/Dota2-API/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "4593c99ba081ef602c49",
+    "url": "/Dota2-API/static/css/main.fa360f35.chunk.css"
+  },
+  {
+    "revision": "d1d1e49540916fd05ba1",
+    "url": "/Dota2-API/static/js/2.a6d7e3b3.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/Dota2-API/static/js/2.a6d7e3b3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4593c99ba081ef602c49",
+    "url": "/Dota2-API/static/js/main.816e162d.chunk.js"
+  },
+  {
+    "revision": "66d1a5a2d77d8b72654c",
+    "url": "/Dota2-API/static/js/runtime-main.8dc3e327.js"
+  },
+  {
+    "revision": "4ecebcfee4d3c934057b8b2d3be4ec9a",
+    "url": "/Dota2-API/static/media/background.4ecebcfe.jpg"
+  },
+  {
+    "revision": "819006401a4e4ac13bab5f2bcb18dfb7",
+    "url": "/Dota2-API/static/media/check.81900640.svg"
+  },
+  {
+    "revision": "7934ca6335bf9511877fd1b76b65134a",
+    "url": "/Dota2-API/static/media/filter-icon.7934ca63.svg"
+  },
+  {
+    "revision": "8809027070d6d3cc632c31f5b2195385",
+    "url": "/Dota2-API/static/media/header-logo.88090270.svg"
+  },
+  {
+    "revision": "25461aadcc3bc0f01941d78766aebd9c",
+    "url": "/Dota2-API/static/media/porter-sans-inline-block-webfont.25461aad.ttf"
+  },
+  {
+    "revision": "821bf4ab18c59c37d01619a8becfd293",
+    "url": "/Dota2-API/static/media/porter-sans-inline-block-webfont.821bf4ab.woff"
+  },
+  {
+    "revision": "907a70a6103d0d26d758d5b7563f5a57",
+    "url": "/Dota2-API/static/media/porter-sans-inline-block-webfont.907a70a6.svg"
+  },
+  {
+    "revision": "933c96d654c2fd4675848634f32465d4",
+    "url": "/Dota2-API/static/media/porter-sans-inline-block-webfont.933c96d6.eot"
+  }
+]);
